a9a498abd2d89e442a3a5dd1b7c951a6ec2cf144
Haining Pan <jackpan1994@126.com>
2019-10-08 20:18:53 -0400

    parfeval to plot

--- R_vs_cond.m
+++ R_vs_cond.m
@@ -1,31 +1,39 @@
-ensemblesize=12;
+ensemblesize=100;
 fig=figure;
-tmpcont=figure;
-crit=0;
-filedir='./M80/N4/Gn0.1/';
-alpha1list=0:0.001:1;
-alpha2list=alpha1list;
-parfor index=1:ensemblesize
+
+for index=1:ensemblesize
 %     if mod(index,10)==0
 % %         disp(index)
 %     end
-    l1=load(strcat(filedir,'condmap/condmap',num2str(index),'.mat'));
-    l2=load(strcat(filedir,'eigvalmap/eigvalmap',num2str(index),'.mat'));
-    condmap=l1.condmap;
-    eigvalmap=l2.eigvalmap;
-    [ilist,jlist,klist]=find(eigvalmap); % i is y-axis, j is x-axis
-    figure(tmpcont);
-    [matcont,~]=contour(alpha1list,alpha2list,eigvalmap,[0.5,1.5],'k');
-     [idx,d]=knnsearch(matcont',[alpha1list(jlist);alpha2list(ilist)]');
-     d=tanh(d);  %scale to [0,1]
-    rmap=sparse(ilist,jlist,d,length(alpha1list),length(alpha2list));
-    condzbcp=(rmap>crit).*(eigvalmap==1).*condmap;
-    figure(fig);
+    F(index)=parfeval(@loaddata,2,index);
+end
+h = waitbar(0,'Waiting for FevalFutures to complete...');
+figure(fig);
+for index=1:ensemblesize    
+    [completedIdx,rmap,condzbcp]=fetchNext(F);
+    waitbar(index/ensemblesize,h,sprintf('Latest result: %d',completedIdx));
     scatter(nonzeros(rmap),nonzeros(condzbcp),0.1,'k');
     hold on;
+    
 end
-figure(fig);
 xlabel("R");
 ylabel("G(e^2/h)");
 savefig('R_vs_cond.fig');
 saveas(fig,'R_vs_cond.png');
+
+function [rmap,condzbcp]=loaddata(index)
+crit=0;
+filedir='./M80/N4/Gn0.1/';
+alpha1list=0:0.001:1;
+alpha2list=alpha1list;
+l1=load(strcat(filedir,'condmap/condmap',num2str(index),'.mat'));
+l2=load(strcat(filedir,'eigvalmap/eigvalmap',num2str(index),'.mat'));
+condmap=l1.condmap;
+eigvalmap=l2.eigvalmap;
+[ilist,jlist,~]=find(eigvalmap); % i is y-axis, j is x-axis
+[matcont,~]=contour(alpha1list,alpha2list,eigvalmap,[0.5,1.5],'k');
+ [~,d]=knnsearch(matcont',[alpha1list(jlist);alpha2list(ilist)]');
+ d=tanh(d);  %scale to [0,1]
+rmap=sparse(ilist,jlist,d,length(alpha1list),length(alpha2list));
+condzbcp=(rmap>crit).*(eigvalmap==1).*condmap;
+end
