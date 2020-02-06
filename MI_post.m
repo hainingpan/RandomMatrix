871c994e034b3cb3b2a881ac3f4f97b17d2d8f88
Haining Pan <jack_pan1994@126.com>
2020-02-06 09:25:02 -0500

    update sweepLMI

--- MI_post.m
+++ MI_post.m
@@ -1,2 +1,6 @@
-load('D:\CMTC\Rp_RM\Rp17\L2sweeptLMI.mat');
-mean(NMIvstlist,2)
+sweeptdata{1}=load('D:\CMTC\Rp_RM\Rp17\L2sweeptLMI.mat');
+sweeptdata{2}=load('D:\CMTC\Rp_RM\Rp17\L5sweeptLMI.mat');
+
+figure;plot(-1:0.1:1,mean(sweeptdata{1}.NMIvstlist,1));
+hold on;
+plot(-1:0.1:1,mean(sweeptdata{2}.NMIvstlist,1))
---
+++ testmutualinfo.m
@@ -0,0 +1,24 @@
+milist=zeros(1000,11);
+rlist=0:0.1:1;
+for i=1:size(milist,1)
+    for j=1:size(milist,2)
+        milist(i,j)=MI(rlist(j));
+    end
+end
+
+function mi=MI(r)
+mux=0;
+muy=0;
+sigmax=1;
+sigmay=1;
+% r=0.9;
+
+mu=[mux,muy];
+sigma=[sigmax^2,r*sigmax*sigmay;r*sigmax*sigmay,sigmay^2];
+list=mvnrnd(mu,sigma,4000);
+
+gL=list(:,1);
+gR=list(:,2);
+
+mi=mutualinfo(gL,gR);
+end
