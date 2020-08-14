cd73b31c2cc800b1e479130820c56ede872aa1e8
Haining Pan <jack_pan1994@126.com>
2020-08-14 13:58:57 -0400

    Update stat.m

--- stat.m
+++ stat.m
@@ -29,54 +29,54 @@ for index=1:ensemblesize/batchsize
        fprintf("%f\n",index/ensemblesize*batchsize);
 end
 
-%Conductance statistics
-edges=linspace(0,4,21); % needs to manually match loaddata.m
-fig1=figure;
-histogram('BinEdges',edges,'BinCounts',mean(condLensemble,1),'FaceColor','r','FaceAlpha',0.4,'DisplayName','G_{LL}');
-xlabel('$G(\frac{e^2}{h})$','Interpreter','latex');
-ylabel('Probility');
-title(sprintf("t=%.2f,L=%d Conductance statistics",t,L));
-hold on;
-histogram('BinEdges',edges,'BinCounts',mean(condRensemble,1),'FaceColor','b','FaceAlpha',0.2,'DisplayName','G_{RR}');
-legend
-saveas(fig1,sprintf("cond_t%.2fL%d.png",t,L));
-savefig(fig1,sprintf("cond_t%.2fL%d.fig",t,L));
+% %Conductance statistics
+% edges=linspace(0,4,21); % needs to manually match loaddata.m
+% fig1=figure;
+% histogram('BinEdges',edges,'BinCounts',mean(condLensemble,1),'FaceColor','r','FaceAlpha',0.4,'DisplayName','G_{LL}');
+% xlabel('$G(\frac{e^2}{h})$','Interpreter','latex');
+% ylabel('Probility');
+% title(sprintf("t=%.2f,L=%d Conductance statistics",t,L));
+% hold on;
+% histogram('BinEdges',edges,'BinCounts',mean(condRensemble,1),'FaceColor','b','FaceAlpha',0.2,'DisplayName','G_{RR}');
+% legend
+% saveas(fig1,sprintf("cond_t%.2fL%d.png",t,L));
+% savefig(fig1,sprintf("cond_t%.2fL%d.fig",t,L));
 
-%Correlation 
-fig2=figure;
-histogram(corensemble,'Normalization','probability');
-xlabel('Correlation');
-ylabel('Probability');
-title(sprintf("t=%.2f,L=%d Correlation statistics",t,L));
-saveas(fig2,sprintf("Cor_t%.2fL%d.png",t,L));
-savefig(fig2,sprintf("Cor_t%.2fL%d.fig",t,L));
+% %Correlation 
+% fig2=figure;
+% histogram(corensemble,'Normalization','probability');
+% xlabel('Correlation');
+% ylabel('Probability');
+% title(sprintf("t=%.2f,L=%d Correlation statistics",t,L));
+% saveas(fig2,sprintf("Cor_t%.2fL%d.png",t,L));
+% savefig(fig2,sprintf("Cor_t%.2fL%d.fig",t,L));
 
 
-%Conductance with 10%
-fig3=figure;
-histogram(cond10Lensemble,'Normalization','probability','FaceColor','r','FaceAlpha',0.4,'DisplayName','G_{LL}');
-hold on;
-histogram(cond10Rensemble,'Normalization','probability','FaceColor','b','FaceAlpha',0.2,'DisplayName','G_{RR}');
-xlabel('Fraction of the nearly quantized region');
-ylabel('Probility');
-title(sprintf("t=%.2f,L=%d Conductance within 10%% of 2 e^2/h",t,L));
-legend;
-saveas(fig3,sprintf("Cond10_t%.2fL%d.png",t,L));
-savefig(fig3,sprintf("Cond10_t%.2fL%d.fig",t,L));
+% %Conductance with 10%
+% fig3=figure;
+% histogram(cond10Lensemble,'Normalization','probability','FaceColor','r','FaceAlpha',0.4,'DisplayName','G_{LL}');
+% hold on;
+% histogram(cond10Rensemble,'Normalization','probability','FaceColor','b','FaceAlpha',0.2,'DisplayName','G_{RR}');
+% xlabel('Fraction of the nearly quantized region');
+% ylabel('Probility');
+% title(sprintf("t=%.2f,L=%d Conductance within 10%% of 2 e^2/h",t,L));
+% legend;
+% saveas(fig3,sprintf("Cond10_t%.2fL%d.png",t,L));
+% savefig(fig3,sprintf("Cond10_t%.2fL%d.fig",t,L));
 
-%ZBCP
-fig4=figure;
-histogram(ZBCPensemble,'Normalization','probability');
-xlabel('Fraction of the ZBCP');
-ylabel('Probility');
-title(sprintf("t=%.2f,L=%d ZBCP statistics",t,L));
-saveas(fig4,sprintf("ZBCP_t%.2fL%d.png",t,L));
-savefig(fig4,sprintf("ZBCP_t%.2fL%d.fig",t,L));
+% %ZBCP
+% fig4=figure;
+% histogram(ZBCPensemble,'Normalization','probability');
+% xlabel('Fraction of the ZBCP');
+% ylabel('Probility');
+% title(sprintf("t=%.2f,L=%d ZBCP statistics",t,L));
+% saveas(fig4,sprintf("ZBCP_t%.2fL%d.png",t,L));
+% savefig(fig4,sprintf("ZBCP_t%.2fL%d.fig",t,L));
 
 %Mutual information
 fig5=figure;
 histogram(miensemble,'Normalization','probability');
-xlabel('Fraction of the ZBCP');
+xlabel('Mutual information');
 ylabel('Probility');
 title(sprintf("t=%.2f,L=%d Mutual information (mean=%f) statistics",t,L,mean(miensemble)));
 saveas(fig5,sprintf("MI_t%.2fL%d.png",t,L));
@@ -85,7 +85,7 @@ savefig(fig5,sprintf("MI_t%.2fL%d.fig",t,L));
 %Normalized mutual information
 fig6=figure;
 histogram(miensemble./jeensemble,'Normalization','probability');
-xlabel('Fraction of the ZBCP');
+xlabel('Normalized mutual information');
 ylabel('Probility');
 title(sprintf("t=%.2f,L=%d Normalized mutual information (mean=%f) statistics",t,L,mean(miensemble./jeensemble)));
 saveas(fig6,sprintf("NMI_t%.2fL%d.png",t,L));
